8f1ce3ad686671e0ccf64bb813a45bbfee64b559
chuckkainrath <Chuck Kainrath>
2021-07-27 15:00:47 -0500

    created splash page

---
+++ react-app/src/components/SplashPage/SplashPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function SplashPage() {
+
+    return (
+        <div>
+            <NavLink to='/login'>Login</NavLink>
+            <NavLink to='/sign-up'>Sign Up</NavLink>
+        </div>
+    );
+}
+
+export default SplashPage;
