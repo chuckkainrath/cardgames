b84951fbf10ed9d2b5538d05d0709febad5daebc
chuckkainrath <Chuck Kainrath>
2021-07-27 14:32:51 -0500

    added wins/losses to user model

--- app/models/user.py
+++ app/models/user.py
@@ -2,32 +2,37 @@ from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+
 class User(db.Model, UserMixin):
-  __tablename__ = 'users'
+    __tablename__ = 'users'
 
-  id = db.Column(db.Integer, primary_key = True)
-  username = db.Column(db.String(40), nullable = False, unique = True)
-  email = db.Column(db.String(255), nullable = False, unique = True)
-  hashed_password = db.Column(db.String(255), nullable = False)
-
-
-  @property
-  def password(self):
-    return self.hashed_password
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(40), nullable = False, unique = True)
+    email = db.Column(db.String(255), nullable = False, unique = True)
+    hashed_password = db.Column(db.String(255), nullable = False)
+    wins = db.Column(db.Integer, default=0)
+    losses = db.Column(db.Integer, default=0)
 
 
-  @password.setter
-  def password(self, password):
-    self.hashed_password = generate_password_hash(password)
+    @property
+    def password(self):
+      return self.hashed_password
 
 
-  def check_password(self, password):
-    return check_password_hash(self.password, password)
+    @password.setter
+    def password(self, password):
+      self.hashed_password = generate_password_hash(password)
 
 
-  def to_dict(self):
-    return {
-      "id": self.id,
-      "username": self.username,
-      "email": self.email
-    }
+    def check_password(self, password):
+      return check_password_hash(self.password, password)
+
+
+    def to_dict(self):
+      return {
+        "id": self.id,
+        "username": self.username,
+        "email": self.email,
+        "wins": self.wins,
+        "losses": self.losses
+      }
