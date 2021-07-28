4cbe9cb6570e8371ae719ef8a3a1d87d36667706
chuckkainrath <Chuck Kainrath>
2021-07-28 13:34:03 -0500

    created socket room join and leave

--- app/socket.py
+++ app/socket.py
@@ -1,4 +1,4 @@
-from flask_socketio import SocketIO, join_room, leave_room, send, emit
+from flask_socketio import SocketIO, join_room, leave_room, emit
 import os
 from app.models import db, User
 import json
@@ -14,4 +14,29 @@ else:
 # Create SocketIO instance
 socketio = SocketIO(cors_allowed_origins=origins)
 
+rooms = []
 
+
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    joined = False
+    for i in range(len(rooms)):
+        if rooms[i] < 4:
+            rooms[i] += 1
+            join_room(i)
+            joined = True
+            break
+
+    if not joined:
+        rooms.append(1)
+        join_room(i + 1)
+
+    emit('player_joined', { 'username': username }, to=str(i))
+
+
+@socketio.on('leave')
+def on_leave(data):
+    room = data['room']
+    leave_room(room)
+    rooms[room] -= 1
