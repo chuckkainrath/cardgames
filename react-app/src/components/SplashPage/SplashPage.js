69e20192286d059155dc9c4e63e170ced50c9c9f
Tristan San Juan <tristansj88@gmail.com>
2021-08-01 07:29:38 -0700

    added features section in splash

--- react-app/src/components/SplashPage/SplashPage.js
+++ react-app/src/components/SplashPage/SplashPage.js
@@ -32,27 +32,78 @@ function SplashPage() {
         ></iframe> */}
 
         <iframe
-          width="100%"
-          height="100%"
-          src="https://www.youtube.com/embed/NiVdFpawZs8?&controls=0&autoplay=1&cc_load_policy=3&mute=1&showinfo=0&autohite=1&rel=0&modestbranding=1&loop=1&playlist=NiVdFpawZs8"
+          src="https://www.youtube.com/embed/NiVdFpawZs8?&controls=0&autoplay=1&cc_load_policy=3&mute=1&showinfo=0&autohite=1&rel=0&modestbranding=1&loop=1&playlist=NiVdFpawZs8&allowfullscreen=1"
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen="allowfullscreen"
         ></iframe>
-        <div class="overlay"></div>
-        <div class="splash-text">
-          <h2>
-            BlaK-JaK
-          </h2>
+        <div className="overlay"></div>
+        <div className="splash-text">
+          <h2>BlaK-JaK</h2>
           <p>
-          It's not about the cards you're dealt, But how you play the hand!
+            It's not about the cards you're dealt, But how you play the hand!
           </p>
           <div className="splash-buttons">
             {" "}
             <NavLink to="/login">Log In</NavLink>
             {"  "} {/*  do not remove space */}
             <NavLink to="sign-up">Sign Up</NavLink>
+          </div>
+          <div className="features-container">
+            <h1>FEATURES</h1>
+            <div className="features-one">
+              <img
+                src="https://media.giphy.com/media/26ufcZICbgCSGe5sQ/giphy.gif"
+                alt=""
+                width="400px"
+                height="300px"
+              ></img>
+              <p>
+                When you arrived in landing page you can either log-in as a
+                existing user or sign-in as a new register user.
+              </p>
+            </div>
+            <div className="features-two">
+              <p>
+                After you log-in or sign-in you can see the be at home page.{" "}
+                <br></br>
+                Where you will see your record on nav bar as well quick links to
+                single and muli-player more
+                <br></br>and the leader board as well as buttons to play in
+                single or multi player modes.
+              </p>
+              <img
+                src="https://media.giphy.com/media/26ufcZICbgCSGe5sQ/giphy.gif"
+                alt=""
+                width="400px"
+                height="300px"
+              ></img>
+            </div>
+            <div className="features-three">
+              <img
+                src="https://media.giphy.com/media/26ufcZICbgCSGe5sQ/giphy.gif"
+                alt=""
+                width="400px"
+                height="300px"
+              ></img>{" "}
+              <p>
+                When you select the single player mode you be playing against
+                the dealer AI.
+              </p>
+            </div>
+            <div className="features-four">
+              <p>
+                In multi-player mode, you can then play with other players going
+                against the dealer.
+              </p>
+              <img
+                src="https://media.giphy.com/media/26ufcZICbgCSGe5sQ/giphy.gif"
+                alt=""
+                width="400px"
+                height="300px"
+              ></img>{" "}
+            </div>
           </div>
         </div>
       </section>
