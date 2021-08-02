b3fde5e06fb1c3863224407c1024133b16dd292f
Tristan San Juan <tristansj88@gmail.com>
2021-08-02 05:09:48 -0700

    styled the log-in form

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
-import  { useDispatch, useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect, NavLink } from "react-router-dom";
 import { login } from "../../store/session";
+import logo from "./bjkids.png";
 
 const LoginForm = () => {
   const dispatch = useDispatch();
-  const user = useSelector(state => state.session.user);
+  const user = useSelector((state) => state.session.user);
   const [errors, setErrors] = useState([]);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -31,34 +32,76 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error) => (
-          <div>{error}</div>
-        ))}
+    <div>
+      <div className="grid min-h-screen place-items-center">
+        <div className="w-11/12 p-12 bg-white sm:w-8/12 md:w-1/2 lg:w-5/12">
+          <h1 className="text-xl font-semibold text-center flex align-items-center flex-col ">
+            <img
+              src={logo}
+              alt="logo"
+              width="100px"
+              height="100px"
+              className="place-self-center "
+            />{" "}
+            BLAK-JAK
+            <p className="font-normal p-5 text-center ">
+              <br></br> Welcome back! Please enter your info to log-in.
+            </p>
+          </h1>
+          <form className="mt-6" onSubmit={onLogin}>
+            <div>
+              {errors.map((error) => (
+                <div>{error}</div>
+              ))}
+            </div>
+            <label
+              for="email"
+              className="block mt-2 text-xs font-semibold text-gray-600 uppercase"
+            >
+              E-mail
+            </label>
+            <input
+              id="email"
+              onChange={updateEmail}
+              value={email}
+              type="email"
+              name="email"
+              placeholder="john.doe@company.com"
+              autocomplete="email"
+              className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner"
+              required
+            />
+            <label
+              for="password"
+              className="block mt-2 text-xs font-semibold text-gray-600 uppercase"
+            >
+              Password
+            </label>
+            <input
+              id="password"
+              onChange={updatePassword}
+              value={password}
+              type="password"
+              name="password"
+              placeholder="********"
+              autocomplete="new-password"
+              className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner"
+              required
+            />
+
+            <button
+              type="submit"
+              className="w-full py-3 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none"
+            >
+              Log-in
+            </button>
+            <p className="flex justify-between inline-block mt-4 text-xs text-gray-500 cursor-pointer hover:text-black">
+              <NavLink to="/sign-up">Not a registered user?</NavLink>
+            </p>
+          </form>
+        </div>
       </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          name="email"
-          type="text"
-          placeholder="Email"
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type="submit">Login</button>
-      </div>
-    </form>
+    </div>
   );
 };
 
